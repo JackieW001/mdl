938b3d4d0551ed39151d187c23e8aabfec93ec78
JackieW001 <jwoo@stuy.edu>
2018-05-15 20:42:41 -0400

    added sanitize

--- script.py
+++ script.py
@@ -3,7 +3,18 @@ from display import *
 from matrix import *
 from draw import *
 
-stack = []
+
+def sanitize(command):
+    if (command[0] == "rotate" or command[0] =="save" or command == "display"):
+        return command
+    ret = [command[0]]
+    i = 1
+    while i < len(command):
+        if isinstance(command[i], float):
+            ret.append(command[i])
+        i+=1
+    return tuple(ret)
+
 
 
 def run(filename):
@@ -33,115 +44,79 @@ def run(filename):
                 0.5]
 
     color = [0, 0, 0]
-    tmp = new_matrix()
-    ident( tmp )
+    polygons = new_matrix()
+    ident( polygons )
 
-    systems = [ [x[:] for x in tmp] ]
+    systems = [ [x[:] for x in polygons] ]
     screen = new_screen()
     zbuffer = new_zbuffer()
     polygons = []
-    step_3d = 20
+    step_3d = 30
+
+    edges = []
 
     p = mdl.parseFile(filename)
-
+    
     if p:
         (commands, symbols) = p
-        for tup in commands:
-            line = tup[0]
-            if line == 'sphere':
-                #print 'SPHERE\t' + str(tup)
+        for each in commands:
+            each  = sanitize(each)
+            if each[0] == "sphere":
                 add_sphere(polygons,
-                       float(tup[1]), float(tup[2]), float(tup[3]),
-                       float(tup[4]), step_3d)
+                           float(each[1]), float(each[2]), float(each[3]),
+                           float(each[4]), step_3d)
                 matrix_mult( systems[-1], polygons )
                 draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
                 polygons = []
-
-            elif line == 'torus':
-                #print 'TORUS\t' + str(tup)
+            elif each[0] == "torus":
                 add_torus(polygons,
-                          float(tup[1]), float(tup[2]), float(tup[3]),
-                          float(tup[4]), float(tup[5]), step_3d)
+                      float(each[1]), float(each[2]), float(each[3]),
+                      float(each[4]), float(each[5]), step_3d)
                 matrix_mult( systems[-1], polygons )
                 draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
                 polygons = []
+            elif each[0] == "box":
 
-            elif line == 'box':
-                #print 'BOX\t' + str(tup)
                 add_box(polygons,
-                        float(tup[1]), float(tup[2]), float(tup[3]),
-                        float(tup[4]), float(tup[5]), float(tup[6]))
+                    float(each[1]), float(each[2]), float(each[3]),
+                    float(each[4]), float(each[5]), float(each[6]))
                 matrix_mult( systems[-1], polygons )
                 draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
                 polygons = []
-                
-            elif line == 'circle':
-                #print 'CIRCLE\t' + str(tup)
-                add_circle(edges,
-                           float(tup[1]), float(tup[2]), float(tup[3]),
-                           float(tup[4]), step)
-                matrix_mult( systems[-1], edges )
-                draw_lines(edges, screen, zbuffer, color)
-                edges = []
-                
-            elif line == 'hermite' or line == 'bezier':
-                #print 'curve\t' + line + ": " + str(tup)
-                add_curve(edges,
-                          float(tup[1]), float(tup[2]),
-                          float(tup[3]), float(tup[4]),
-                          float(tup[5]), float(tup[6]),
-                          float(tup[7]), float(tup[8]),
-                          step, line)
-                matrix_mult( systems[-1], edges )
-                draw_lines(edges, screen, zbuffer, color)
-                edges = []
-                
-            elif line == 'line':
-                #print 'LINE\t' + str(tup)
-                
+            elif each[0] == "line":
                 add_edge( edges,
-                          float(tup[1]), float(tup[2]), float(tup[3]),
-                          float(tup[4]), float(tup[5]), float(tup[6]) )
+                      float(each[1]), float(each[2]), float(each[3]),
+                      float(each[4]), float(each[5]), float(each[6]) )
                 matrix_mult( systems[-1], edges )
-                draw_lines(eges, screen, zbuffer, color)
+                draw_lines(edges, screen, zbuffer, color)
                 edges = []
-                
-            elif line == 'scale':
-                #print 'SCALE\t' + str(tup)
-                t = make_scale(float(tup[0]), float(tup[1]), float(tup[2]))
+            elif each[0] == "move":
+                t = make_translate(float(each[1]), float(each[2]), float(each[3]))
                 matrix_mult( systems[-1], t )
                 systems[-1] = [ x[:] for x in t]
-                
-            elif line == 'move':
-                #print 'MOVE\t' + str(tup)
-                t = make_translate(float(tup[1]), float(tup[2]), float(tup[3]))
+            elif each[0] == "scale":
+                t = make_scale(float(each[1]), float(each[2]), float(each[3]))
                 matrix_mult( systems[-1], t )
                 systems[-1] = [ x[:] for x in t]
-                
-            elif line == 'rotate':
-                #print 'ROTATE\t' + str(tup)
-                theta = float(tup[2]) * (math.pi / 180)
-                if tup[1] == 'x':
+            elif each[0] == "rotate":
+                theta = float(each[2]) * (math.pi / 180)
+                if each[1] == 'x':
                     t = make_rotX(theta)
-                elif tup[1] == 'y':
+                elif each[1] == 'y':
                     t = make_rotY(theta)
                 else:
                     t = make_rotZ(theta)
                 matrix_mult( systems[-1], t )
                 systems[-1] = [ x[:] for x in t]
-                    
-            elif line == 'push':
+            elif each[0] == "push":
                 systems.append( [x[:] for x in systems[-1]] )
-                
-            elif line == 'pop':
+            elif each[0] == "pop":
                 systems.pop()
-                
-            elif line == 'display' or line == 'save':
-                if line == 'display':
-                    display(screen)
-                else:
-                    save_extension(screen, tup[1]+str(tup[2]))
-                
+            elif each[0] == "save":
+                save_extension(screen, each[1]+each[2])
+            elif each[0] == "display":
+                display(screen)
+
     else:
         print "Parsing failed."
         return
